62cf5941090dbb26c6b78790105b11c61da6a099
Sunjae_Kim <kimsunja@grinnell.edu>
2024-09-24 15:43:27 -0500

    Tests for smallerRoot

--- src/test/java/edu/grinnell/csc207/TestQuadratic.java
+++ src/test/java/edu/grinnell/csc207/TestQuadratic.java
@@ -1,11 +1,9 @@
 package edu.grinnell.csc207;
 
-import edu.grinnell.csc207.util.Quadratic;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
+
+import edu.grinnell.csc207.util.Quadratic;
 
 /**
  * Tests of the Quadratic class.
@@ -34,5 +32,18 @@ public class TestQuadratic {
     assertEquals(0, (new Quadratic(1, -3, 0)).smallerRoot(), 0.001,
         "The smaller root of x^2 - 3x is 0");
   } // testRootsNoC()
+
+  @Test
+  public void testSmallerRoot(){
+    //Roots: 1 vs 3
+    //-1 vs -3
+    //1 vs -3
+    //-1 vs 3
+    assertEquals(1, new Quadratic(1, -4, 3).smallerRoot());
+    assertEquals(-1, new Quadratic(1, 2, -3).smallerRoot());
+    assertEquals(-3, new Quadratic(1, 2, -3).smallerRoot());
+    assertEquals(-3, new Quadratic(1, 4, 3).smallerRoot());
+    assertEquals(-1.5, new Quadratic(6, 1, -12).smallerRoot());
+  }
 
 } // class TestQuadratic
