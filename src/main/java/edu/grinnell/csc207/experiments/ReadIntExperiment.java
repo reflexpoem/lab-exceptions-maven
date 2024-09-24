84fd05c9482648a4a6ae937816cfeab9bff15b23
Sunjae_Kim <kimsunja@grinnell.edu>
2024-09-24 15:31:12 -0500

    first part done

--- src/main/java/edu/grinnell/csc207/experiments/ReadIntExperiment.java
+++ src/main/java/edu/grinnell/csc207/experiments/ReadIntExperiment.java
@@ -1,7 +1,5 @@
 package edu.grinnell.csc207.experiments;
 
-import edu.grinnell.csc207.util.IOUtils;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -23,14 +21,32 @@ public class ReadIntExperiment {
    *
    * @throws Exception when something goes wrong.
    */
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args)  {
     PrintWriter pen = new PrintWriter(System.out, true);
     BufferedReader eyes = new BufferedReader(new InputStreamReader(System.in));
+    pen.printf("Please enter an integer: ");
 
-    int i = IOUtils.readInt(pen, eyes, "Please enter an integer: ");
-    pen.printf("%d squared is %d\n", i, i * i);
+    try {
+      String response = eyes.readLine();
+      int i = Integer.parseInt(response);
+      pen.printf("%d squared is %d\n", i, i * i);
+    } catch (Exception e) {
+      int i = Integer.MIN_VALUE;
+      pen.printf("%d squared is %d\n", i, i * i);
+    } // Reading/parsing fails.
 
-    eyes.close();
+    try {
+      eyes.close();
+        
+    } catch (Exception e) {
+      return;
+    }
+
+    //int i = IOUtils.readInt(pen, eyes, "Please enter an integer: ");
+    
+    
+
+    
     pen.close();
   } // main(String[])
 
